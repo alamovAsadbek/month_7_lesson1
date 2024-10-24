f7bfc42295f58bc4bf63096828acaa4b4a3f8c5f
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-24 22:36:55 +0500

    change

--- products/products/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from products import views
-
-app_name = 'products'
-
-urlpatterns = [
-    path('list/', views.ProductListView.as_view(), name='list'),
-    path('detail/', views.ProductDetailView.as_view(), name='detail'),
-]
